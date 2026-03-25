5f155c6e3ff4ab2c5313e83327e4efe1e0728f7c
Aleksandr <ichmarchuk@gail.com>
2026-03-25 12:07:18 +0100

    add mui-header v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import ResponsiveAppBar from "./components/header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ResponsiveAppBar />
+      {/* <Main /> */}
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
